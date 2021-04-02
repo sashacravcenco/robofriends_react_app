3b57159ad1e709e26b9a73876de3eb1efaa1d9e0
splincode <omaxphp@yandex.com>
2021-04-02 22:26:28 +0300

    fixed styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll"
-import "../containers/App.css";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+import "./App.css";
 
 class App extends Component {
     constructor() {
@@ -37,7 +38,9 @@ class App extends Component {
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <Scroll>
+            <ErrorBoundry>
             < CardList robots={filteredRobots}/> 
+            </ErrorBoundry>
             </Scroll>           
             </div>
         );
